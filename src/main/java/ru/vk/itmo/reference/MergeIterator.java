580120b8d1e9e1b3151d50477054225a737c8228
Vladislav Bandurin <v.bandurin@vkteam.ru>
2026-03-17 19:59:44 +0300

    reference stage-3

---
+++ src/main/java/ru/vk/itmo/reference/MergeIterator.java
@@ -0,0 +1,134 @@
+package ru.vk.itmo.reference;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+
+public class MergeIterator<T> implements Iterator<T> {
+
+    private final PriorityQueue<PeekIterator<T>> priorityQueue;
+    private final Comparator<T> comparator;
+    PeekIterator<T> peekIterator;
+
+    public MergeIterator(Collection<Iterator<T>> iterators, Comparator<T> comparator) {
+        this.comparator = comparator;
+        Comparator<PeekIterator<T>> peekComp = (o1, o2) -> comparator.compare(o1.peek(), o2.peek());
+        priorityQueue = new PriorityQueue<>(
+                iterators.size(),
+                peekComp.thenComparing(o -> -o.id)
+        );
+
+        int id = 0;
+        for (Iterator<T> iterator : iterators) {
+            if (iterator.hasNext()) {
+                priorityQueue.add(new PeekIterator<>(id++, iterator));
+            }
+        }
+    }
+
+    private PeekIterator<T> peek() {
+        while (peekIterator == null) {
+            peekIterator = priorityQueue.poll();
+            if (peekIterator == null) {
+                return null;
+            }
+
+            // |     it1    |   it2  |   it3  | ...
+            // | k1, k2, k3 | k2, k4 | k1, k4 | ...
+            while (true) {
+                PeekIterator<T> next = priorityQueue.peek();
+                if (next == null) {
+                    break;
+                }
+
+                int compare = comparator.compare(peekIterator.peek(), next.peek());
+                if (compare == 0) {
+                    PeekIterator<T> poll = priorityQueue.poll();
+                    if (poll != null) {
+                        poll.next();
+                        if (poll.hasNext()) {
+                            priorityQueue.add(poll);
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            if (skip(peekIterator.peek())) {
+                peekIterator.next();
+                if (peekIterator.hasNext()) {
+                    priorityQueue.add(peekIterator);
+                }
+                peekIterator = null;
+            }
+        }
+
+        return peekIterator;
+    }
+
+    protected boolean skip(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return peek() != null;
+    }
+
+    @Override
+    public T next() {
+        PeekIterator<T> peek = peek();
+        if (peek == null) {
+            throw new NoSuchElementException();
+        }
+        T next = peek.next();
+        this.peekIterator = null;
+        if (peek.hasNext()) {
+            priorityQueue.add(peek);
+        }
+        return next;
+    }
+
+    private static class PeekIterator<T> implements Iterator<T> {
+
+        public final int id;
+        private final Iterator<T> delegate;
+        private T peekSegment;
+
+        private PeekIterator(int id, Iterator<T> delegate) {
+            this.id = id;
+            this.delegate = delegate;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (peekSegment == null) {
+                return delegate.hasNext();
+            }
+            return true;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T peek = peek();
+            this.peekSegment = null;
+            return peek;
+        }
+
+        private T peek() {
+            if (peekSegment == null) {
+                if (!delegate.hasNext()) {
+                    return null;
+                }
+                peekSegment = delegate.next();
+            }
+            return peekSegment;
+        }
+    }
+}
